3876944b5c5478e95c7263875ee932fdf119d565
Shubham Dawra <k00lhunt3rz@gmail.com>
2022-06-02 21:48:18 +0530

    Updated font files

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 class Config{
-    static ffmpeg_path = "C:\\Users\\shubh\\Downloads\\ffmpeg-20180209-e752da5-win64-static (1)\\ffmpeg-20180209-e752da5-win64-static\\bin\\ffmpeg";
+    static ffmpeg_path = "E:\\ffmpeg\\bin\\ffmpeg";
     static convert_path = "C:\\Program Files\\ImageMagick-7.1.0-Q16\\magick";
     static magick_path = "C:\\Program Files\\ImageMagick-7.1.0-Q16\\magick";
     static output_directory = path.join(__dirname, 'download')+path.sep;
-    static roboto_bold = "F:\\Font\\Roboto-Bold.ttf";
-    static roboto_light = "F:\\assets\\Font\\Roboto-Light.ttf";
+    static roboto_bold = "E:\\animoto_reference\\Animated-VideoMaker\\assets\\Font\\Roboto-Bold.ttf";
+    static roboto_light = "E:\\animoto_reference\\Animated-VideoMaker\\assets\\Font\\Roboto-Light.ttf";
+    static roboto_bold_metric = "E:\\animoto_reference\\Animated-VideoMaker\\fontcode\\robotobold.json";
+    static roboto_light_metric = "E:\\animoto_reference\\Animated-VideoMaker\\fontcode\\robotolight.json";
     static audio = "F:\\assets\\Audio\\audio.mpeg";
-    static socket_ip="192.168.1.107:82";
+    static socket_ip="192.168.1.103:82";
     static users=[];
     static connecton=null;
     static ch=null;
